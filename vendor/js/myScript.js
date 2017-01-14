ad3185fd2a1541a2d1ce5189ef162f58f2c9f39a
Zyrass <zyrass@outlook.fr>
2017-01-14 16:49:54 +0100

    envoi des dernières modifications

--- vendor/js/myScript.js
+++ vendor/js/myScript.js
@@ -1,11 +1,16 @@
 
-$(function(){
-    $(".navbar a, footer a").on("click", function(event){
+$(document).ready(function(){
 
+    $('a[href^="#"]').on('click', function(event) {
         event.preventDefault();
-        var hash = $(this).hash;
 
-        $('body').animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
+        var hash = $(this.hash);
+
+        $('body').stop().animate( {
+            'scrollTop' : hash.offset().top
+        }, 900, function(){
+            window.location.hash = hash;
+        })
     });
 
 
